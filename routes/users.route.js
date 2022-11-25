d0760a9ff2d7f5743fd68b1e807461414d9ebc13
raflyafrzl <comfortyourstep@gmail.com>
2022-11-25 22:41:24 +0700

    feat:completing PUT request

--- routes/users.route.js
+++ routes/users.route.js
@@ -3,7 +3,7 @@ const authMiddleware = require("../middlewares/auth.middleware");
 
 const userController = require("../controllers/users.controller");
 
-router.route("/").put(authMiddleware, userController.updateUser);
+router.route("/:userId").put(authMiddleware, userController.updateUser);
 
 router.route("/register").post(userController.createUser);
 router.route("/login").post(userController.loginUser);
