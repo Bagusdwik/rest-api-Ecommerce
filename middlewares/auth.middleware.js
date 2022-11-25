e78274a424bd7d593dca177053f509db917dcfe1
raflyafrzl <comfortyourstep@gmail.com>
2022-11-25 22:29:06 +0700

    feat:adding authorization for PUT request

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const { User } = require("../models/index");
-const AppError = require("../utils/app-error");
+const AppError = require("../utils/app.error.middleware");
 async function authMiddleware(req, res, next) {
   //write logic code here
 
@@ -8,7 +8,7 @@ async function authMiddleware(req, res, next) {
     const token = req.headers["x-access-token"].split(" ");
     if (token[0] !== "Bearer") throw new Error("Invalid token");
 
-    const result = jwt.verify(token[1], process.env.JWT_TOKEN);
+    const result = jwt.verify(token[1], process.env.JWT_SECRET_KEY);
 
     // const resultData = await User.findAll({
     //   where: {
